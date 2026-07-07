74f3c18ab580bfec844619497e561179d9dcb7cd
ahmedmakty313gaming <ahmedmakty313@gmail.com>
2026-07-07 13:09:53 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name:ahmedmakty313",
+        name:"ahmedmakty313",
         // Activity type number (0 = Playing).
         type: 2,
       },
