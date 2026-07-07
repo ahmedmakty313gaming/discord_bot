c723c764f8948d122a85f02bdd5b072e39173342
ahmedmakty313gaming <ahmedmakty313@gmail.com>
2026-07-07 18:21:35 +0300

    Update beg.js

--- src/commands/Economy/beg.js
+++ src/commands/Economy/beg.js
@@ -5,7 +5,7 @@ import { botConfig } from '../../config/bot.js';
 import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const COOLDOWN = 30 * 60 * 1000;
+const COOLDOWN = 3 * 6 * 10;
 const MIN_WIN = 50;
 const MAX_WIN = 200;
 const SUCCESS_CHANCE = 0.7;
