ca5d300387091d283d5f2b2bc03a5bac146fc0f3
ahmedmakty313gaming <ahmedmakty313@gmail.com>
2026-07-07 18:25:09 +0300

    Update beg.js

--- src/commands/Economy/beg.js
+++ src/commands/Economy/beg.js
@@ -5,10 +5,10 @@ import { botConfig } from '../../config/bot.js';
 import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const COOLDOWN = 3 * 6 * 10;
+const COOLDOWN = 30 * 60 * 1000;
 const MIN_WIN = 50;
 const MAX_WIN = 200;
-const SUCCESS_CHANCE = 0.7;
+const SUCCESS_CHANCE = 0.9;
 
 export default {
     data: new SlashCommandBuilder()
